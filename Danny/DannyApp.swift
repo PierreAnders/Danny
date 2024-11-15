f1882d9cb6b7d56901867792efbe405c402766af
PierreAnders <pierre.untas@gmail.com>
2024-11-15 22:33:01 +0100

    Initial Commit

---
+++ Danny/DannyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DannyApp.swift
+//  Danny
+//
+//  Created by Pierre Untas on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct DannyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DannyTests/DannyTests.swift
@@ -0,0 +1,17 @@
+//
+//  DannyTests.swift
+//  DannyTests
+//
+//  Created by Pierre Untas on 15/11/2024.
+//
+
+import Testing
+@testable import Danny
+
+struct DannyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
